8d2bda6c81ed1189f0c83c13e7ba884941b11519
Cassiuslangu <m.cassiuslangu@gmail.com>
2021-06-28 11:04:11 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char charType;
+int intType;
+float floatType;
+
+printf("Size of a char: %c byte(s)\n", sizeof(charType));
+printf("Size of an int: %i byte(s)\n", sizeof(intType));
+printf("Size of a long int: %ld byte(s)\n", sizeof(intType));
+printf("Size of a long long int: %lld byte(s)\n", sizeof(intType));
+printf("Size of a float: %f byte(s)\n", sizeof(floatType));
+return (0);
+}
