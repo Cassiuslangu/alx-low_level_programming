167c3eeab89057e2d91f72eb62937b130b4dab0f
Cassiuslangu <m.cassiuslangu@gmail.com>
2021-06-28 11:32:02 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ float floatType;
 
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-printf("Size of a long int: %li byte(s)\n", sizeof(longintType));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongintType));
+printf("Size of a long int: %lu byte(s)\n", sizeof(longintType));
+printf("Size of a long long int: %li byte(s)\n", sizeof(longlongintType));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
