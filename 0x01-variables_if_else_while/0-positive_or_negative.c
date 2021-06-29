047f07b5ea496d25c8918ac0b2eb639eb24ecd2b
Cassiuslangu <m.cassiuslangu@gmail.com>
2021-06-29 09:53:00 -0700

    Positive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * 0-positive_or_negative
+ * if the number is greater than 0: is positive\n
+ * if the number is 0: is zero\n
+ * if the number is less than 0: is negative\n
  * Return: Always 0 (Success)
  */
 
@@ -10,7 +12,7 @@ int main(void)
 {
 int n;
 
-scanf("%i", &n);
+scanf("%d", &n);
 if (n > 0)
 printf("%d is positive\n", n);
 else if (n < 0)
