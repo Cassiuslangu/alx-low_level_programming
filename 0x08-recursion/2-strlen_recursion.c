d7a5d3b8bc4f31221ddc4b26959557bb98fee2b7
Cassiuslangu <m.cassiuslangu@gmail.com>
2021-07-11 06:10:13 -0700

    Print the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * strlen_recursion -returns the lenght of a string
+ * @s: string to calculate the length of a string
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+  if(*s == '\0')
+    return (0);
+  return (1 + _strlen_recursion(s + 1));
+}
