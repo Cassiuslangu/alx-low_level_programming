09e78b8c1ebd89d0f31bb2a81053ffddc7a14aed
Cassiuslangu <m.cassiuslangu@gmail.com>
2021-07-11 06:12:42 -0700

    Print the length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,13 +3,13 @@
 
 /**
  * strlen_recursion -returns the lenght of a string
- * @s: string to calculate the length of a string
+ * @s: output of the length of a string
  * Return: length of the string
  */
 
 int _strlen_recursion(char *s)
 {
-  if(*s == '\0')
-    return (0);
-  return (1 + _strlen_recursion(s + 1));
+if(*s == '\0')
+return (0);
+return (1 + _strlen_recursion(s + 1));
 }
