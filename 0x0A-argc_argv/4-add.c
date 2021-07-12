0f3b7eff386a47ee1dbd7284d81a50964463e29a
Cassiuslangu <m.cassiuslangu@gmail.com>
2021-07-12 14:16:37 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,7 +12,7 @@ int _atoi(char *s)
 short boolean;
 int i, minus result;
 
-i = minus = result = boolean =0;
+i = minus = result = boolean = 0;
 minus = -1;
 
 while (s[i] != '\0')
@@ -20,26 +20,26 @@ while (s[i] != '\0')
 if (s[i] == '-')
 minus *= -1;
 
-if (s[i] > = '0' && s[i] <= '9')
+if (s[i] >= '0' && s[i] <= '9')
 {
 result *= 10;
 result -= (s[[i] - '0');
 boolean = 1;
 }
 else if (boolean == 1)
-braek;
+break;
 i++;
 }
 result *= minus;
 return (result);
 }
 
- /**
- * main -Program that adds positive numbers.
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 or 1
- */
+/**
+* main -Program that adds positive numbers.
+* @argc: argument count
+* @argv: argument vector
+* Return: 0 or 1
+*/
 
 int main(int argc, char *argv[])
 {
@@ -47,6 +47,17 @@ int sum, num, i, j, k;
 
 sum = 0;
 
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] > '9' || argv[i][j] < '0')
+{
+puts("Error");
+return (1);
+}
+}
+}
 for (k = 1; k < argc; k++)
 {
 num = _atoi(argv[k]);
