f517a7ed24722c34ee4d57d1ea05bd45b7921960
Cassiuslangu <m.cassiuslangu@gmail.com>
2021-07-12 09:37:06 -0700

    What is my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * main - print the nae of the name of the program
  * @argc: argument count
  * @argc: argument vector
- * Return: Always 0
+ * Return: 0
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
