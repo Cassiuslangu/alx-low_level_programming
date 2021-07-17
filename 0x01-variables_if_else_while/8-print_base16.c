d0331926f9295bada88c4f331f1fc083761c3fa2
Cassiuslangu <m.casiuslangu@gmail.com>
2021-07-17 14:18:00 +0200

    Print all numbers with base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+  
+#include <stdio.h>
+
+/**
+ * main - Program will print all the numbers of base 16.
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
