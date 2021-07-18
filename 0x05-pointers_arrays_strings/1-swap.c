2e5236a2c875f53755b9acaeda835043f68a6957
Cassiuslangu <m.casiuslangu@gmail.com>
2021-07-18 17:44:41 +0200

    Swaps the values of two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer first value
+ * @b: pointer second value
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
