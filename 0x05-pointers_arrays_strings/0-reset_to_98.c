9a7d220e5458aac8c2733c7cdcb7dbe152de51c9
Cassiuslangu <m.casiuslangu@gmail.com>
2021-07-18 17:29:01 +0200

    Pointer to an int

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,20 +1,12 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
-* main - check the code for ALX School students.
-*
+* reset_to_98 - updates the value it points to to 98.
+* @n: Pointer
 * Return: Always 0.
 */
 
-int main(void)
-{
-int n;
-
-n = 402
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
 void reset_to_98(int *n)
-return (0);
+{
+	*n = 98;
 }
