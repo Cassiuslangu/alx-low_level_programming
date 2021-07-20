f522879ff4eb893fe40d42dd586c3ba83c45a5f8
Cassiuslangu <m.casiuslangu@gmail.com>
2021-07-20 19:59:03 +0200

    Print the name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints out a name using a given callback
+ * @name: name to print
+ * @f: function callback
+ * Return: (void)
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+        if (name != NULL)
+                (*f)(name);
+}
