59db409013194458ba0d90815916352532646b4c
Cassiuslangu <m.casiuslangu@gmail.com>
2021-07-20 14:03:56 +0200

    Creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,8 +2,24 @@
 #include "dog.h"
 
 /**
- * dog_t *my_dog - Creates a new dog
- * 
- * Return: Nothing
+ * dog_t *my_dog - typedef new name
+ * Return: NULL
  */
 
+dog_t *new_dog(char *name, float age, char *owner);
+{
+	char *name;
+	float age;
+	char *owner;
+
+    dog_t *new_dog;
+
+    new_dog = malloc (sizeof(dog_t));
+    if (new_dog == NULL)
+    return (NULL)
+
+    new_dog->name = name;
+    new_dog->age = age;
+    new_dog->owner = owner;
+    return new_dog;
+}
