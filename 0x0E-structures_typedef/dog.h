ba12650d38585dfb6fdfe6ed56c5cad438897124
Cassiuslangu <m.casiuslangu@gmail.com>
2021-07-21 10:55:19 +0200

    Prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,7 +10,7 @@
  * Reyurn: Always 0
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
@@ -21,7 +21,7 @@ struct dog
  * dog_t - typedef for struct dog
  */
 
-struct dog dog_t;
+struct dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
