a0c61475e008ea1a37f2f36e0f989ce3dbb92c67
Cassiuslangu <m.casiuslangu@gmail.com>
2021-07-22 14:34:33 +0200

    Prototype function

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,6 @@
 #ifndef FILE_FUNCTION_POINTERS_H
 #define FILE_FUNCTION_POINTERS_H
 
-int _putchar
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
@@ -12,4 +11,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
-#endif /* FUNCTION_POINTERS_H */
+#endif /* FILE_FUNCTION_POINTERS_H */
