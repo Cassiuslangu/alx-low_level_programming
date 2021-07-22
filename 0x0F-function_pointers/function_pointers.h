a66d0f47e8b775b8a784938f6fcd988638d5672b
Cassiuslangu <m.casiuslangu@gmail.com>
2021-07-22 14:30:09 +0200

    Prototype function

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FILE_FUNCTION_POINTERS_H
+#define FILE_FUNCTION_POINTERS_H
 
 int _putchar
 void print_name(char *name, void (*f)(char *));
