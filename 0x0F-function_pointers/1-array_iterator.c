2462bb2289ef4da31f32648471b9eea35b994f1c
Cassiuslangu <m.casiuslangu@gmail.com>
2021-07-22 15:46:48 +0200

    Executes a function given as a parameter

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,14 +11,14 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-        if (array && action)
-        {
-                unsigned int i = 0;
-                
-                while (i < size)
-        {
-                (*action)(array[i]);
-                i++;
-        }
-        }
+	if (array && action)
+	{
+		unsigned int i = 0;
+
+		while (i < size)
+		{
+			(*action)(array[i]);
+			i++;
+		}
+	}
 }
