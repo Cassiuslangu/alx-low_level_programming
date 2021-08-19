3a2767f2c204325ef817978e890e42c71ae4620f
Cassiuslangu <m.casiuslangu@gmail.com>
2021-08-19 10:32:57 +0200

    Binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,6 @@
  * @b: string containing the binary number
  * Return: the converted number
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
@@ -20,3 +19,6 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 		dec_val = 2 * dec_val + (b[i] - '0');
 	}
+
+	return (dec_val);
+}
